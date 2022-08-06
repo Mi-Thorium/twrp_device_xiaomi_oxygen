7991db10ef0e7add7857a683334692427adc63aa
me-cafebabe <me_cafebabe@hotmail.com>
2022-08-07 00:50:55 +0800

    oxygen: Add omni lunch target

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -11,3 +11,11 @@ COMMON_LUNCH_CHOICES := \
     twrp_oxygen-user \
     twrp_oxygen-userdebug \
     twrp_oxygen-eng
+
+PRODUCT_MAKEFILES += \
+    $(LOCAL_DIR)/omni_oxygen.mk
+
+COMMON_LUNCH_CHOICES += \
+    omni_oxygen-user \
+    omni_oxygen-userdebug \
+    omni_oxygen-eng
---
+++ omni_oxygen.mk
@@ -0,0 +1,19 @@
+#
+# Copyright (C) 2022 Team Win Recovery Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+PRODUCT_RELEASE_NAME := oxygen
+
+# Inherit from oxygen device
+$(call inherit-product, device/xiaomi/oxygen/device.mk)
+
+# Device identifier. This must come after all inclusions
+PRODUCT_DEVICE := $(PRODUCT_RELEASE_NAME)
+PRODUCT_NAME := omni_$(PRODUCT_RELEASE_NAME)
+BOARD_VENDOR := Xiaomi
+PRODUCT_BRAND := Xiaomi
+PRODUCT_MODEL := MI MAX 2
+PRODUCT_MANUFACTURER := Xiaomi
+TARGET_VENDOR := Xiaomi
